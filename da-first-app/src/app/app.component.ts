b75ae1b9949dd3218f2d6ba99483cb2a131635a0
André Davis <CubicleJockey@users.noreply.github.com>
2022-02-24 13:50:11 -0800

    Try inline template for Servers Component

--- da-first-app/src/app/app.component.ts
+++ da-first-app/src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: 
+  `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
