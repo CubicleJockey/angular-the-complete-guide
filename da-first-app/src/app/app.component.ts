477443f049184813178d29c1a0e455aa36fa7b5d
André Davis <CubicleJockey@users.noreply.github.com>
2022-02-23 23:03:06 -0800

    Check-point

--- da-first-app/src/app/app.component.ts
+++ da-first-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'da-first-app';
+  public name: string = 'André Davis';
 }
