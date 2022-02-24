9d441f914f3ef6920eb4bdd28b1ec37a7d606b02
André Davis <CubicleJockey@users.noreply.github.com>
2022-02-24 15:36:20 -0800

    Assignment 1

---
+++ assignment-1/src/app/warn-alert/warn-alert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: '[app-warn-alert]',
+    template: `<p><span class="badge bg-warning">Oh noes n' stuff!</span></p>`,
+})
+export class WarnAlertComponent{
+
+}
