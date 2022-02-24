deb04ed4295dd66b71ad54b896a44214fb921f4f
André Davis <CubicleJockey@users.noreply.github.com>
2022-02-24 14:11:39 -0800

    Styles

--- da-first-app/src/app/app.component.ts
+++ da-first-app/src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: 
-  `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
