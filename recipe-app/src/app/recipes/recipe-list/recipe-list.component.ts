470c3410c9a6bdb08295a8ebda2e6c4358fd1673
André Davis <CubicleJockey@users.noreply.github.com>
2022-02-28 18:02:02 -0800

    Starting recipe list component

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  public recipes: any = [];
+  public recipes: Recipe[] = [];
 
-  constructor() { }
+  constructor() { 
+    //Sample recipe
+    this.recipes.push(
+      new Recipe('Vegan Tacos Sample'
+                ,'Super tastey vegan street tacos'
+                ,'https://www.acouplecooks.com/wp-content/uploads/2021/01/Tofu-Tacos-023.jpg')
+                
+      );
+  }
 
   ngOnInit(): void {
   }
-
 }
