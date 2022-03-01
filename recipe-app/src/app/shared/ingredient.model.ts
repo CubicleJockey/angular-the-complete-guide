b2b7ada1b21e2f6be6909a3c57d5cd6c307d39b0
André Davis <CubicleJockey@users.noreply.github.com>
2022-03-01 13:06:48 -0800

    1. Setting up Shopping List component structure 2. Modifications to recipe Details

---
+++ recipe-app/src/app/shared/ingredient.model.ts
@@ -0,0 +1,8 @@
+export class Ingredient{
+    // public Name: string;
+    // public Quantity: number;
+    
+    //Putting public in the constructor is a short-cut for creating the property and assigning
+    constructor(public Name: string, public Quantity: number){
+    }
+}
