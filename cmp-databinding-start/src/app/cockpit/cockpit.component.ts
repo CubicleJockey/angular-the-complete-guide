2a44b14deadb1771c3de8da8a05c6f568a2f9d32
André Davis <CubicleJockey@users.noreply.github.com>
2022-03-01 17:39:22 -0800

    Complexity
    ViewChild, ElementRef and ng-content usage.

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -7,10 +7,14 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
   //public newServerName:string = '';
-  public newServerContent:string = '';
+  //public newServerContent:string = '';
 
   @Output() public serverCreated: EventEmitter<{name: string; content: string}>;
   @Output('bpCreated') public blueprintCreated: EventEmitter<{name: string; content: string}>;
+
+  //Get local reference
+  @ViewChild('serverContentInput') 
+  public serverContentInput: ElementRef;
 
   constructor() { 
     this.serverCreated = new EventEmitter<{name: string; content: string}>();
@@ -20,17 +24,17 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddServer(nameInput: HTMLInputElement) {
+  onAddServer(nameInput: HTMLInputElement) {   
      this.serverCreated.emit({ 
         name: nameInput.value
-       ,content: this.newServerContent
+       ,content: this.serverContentInput.nativeElement.value
       });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
        name: nameInput.value
-      ,content: this.newServerContent
+      ,content: this.serverContentInput.nativeElement.value
     });
   }
 }
